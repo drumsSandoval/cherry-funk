7ca941d25893ca43a5d029c61e848cb9137bc265
oscar <oscar@avena.io>
2024-07-16 07:26:49 -0600

    config deployu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import staticSite from '@astrojs/static-site'
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+	site: 'https://cherryfunk.band',
+	integrations: [mdx(), sitemap(), staticSite()],
+	build: {
+		outDir: 'dist'
+	}
+})
