1a50fa5f14abbef6dd5b6eb9de7bfd5910b76509
oscar <oscar@avena.io>
2024-07-16 07:31:21 -0600

    config improve

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import staticSite from '@astrojs/static-site'
 
 export default defineConfig({
 	site: 'https://cherryfunk.band',
-	integrations: [mdx(), sitemap(), staticSite()],
+	integrations: [mdx(), sitemap()],
 	build: {
 		outDir: 'dist'
 	}
